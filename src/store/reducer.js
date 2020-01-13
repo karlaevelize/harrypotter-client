ae88466dc35a2b6d665e5e3f3e2b4099cb63fcbe
karaevelize <k.evelize@gmail.com>
2020-01-13 12:52:34 +0100

    added a login component and a toolbar component

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,7 +1,5 @@
 import { combineReducers } from "redux";
-import signUpReducer from "./signup/reducer";
 import loginReducer from "./login/reducer";
-import chatReducer from "./chat/reducer";
 
 export default combineReducers({
   login: loginReducer
